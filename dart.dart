2bdb80906ce8bfef17b9dd4c2e2ad700d7551adc
briansiky69 <138884235+briansiky69@users.noreply.github.com>
2024-03-22 13:37:37 +0300

    dirt

---
+++ dart.dart
@@ -0,0 +1,24 @@
+void main() {
+  // Integer (int) to store whole numbers
+  int age = 25;
+  print("My age is: $age");
+
+  // Double (double) to store decimal numbers
+  double pi = 3.14159;
+  print("The value of pi is: $pi");
+
+  // String (String) to store text data
+  String name = "Brian Sikuku";
+  print("My name is: $name");
+
+  // List (List) to store an ordered collection of items
+  List<String> colors = ["Red", "Green", "Blue"];
+  print("My favorite colors are: $colors");
+
+  // Map (Map) to store key-value pairs
+  Map<String, String> fruits = {
+    "apple": "red",
+    "banana": "yellow",
+  };
+  print("An apple is: ${fruits["apple"]}"); // Access value by key
+}
